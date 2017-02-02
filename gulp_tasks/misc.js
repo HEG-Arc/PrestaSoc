f3800f659c134fff70104a2e1bce376c4d8e858a
Boris Fritscher <boris@fritscher.ch>
2017-02-02 19:42:24 +0100

    Custom Gulp Task simvars to extarct used vars from html templates

--- gulp_tasks/misc.js
+++ gulp_tasks/misc.js
@@ -23,3 +23,37 @@ function other() {
     .pipe(fileFilter)
     .pipe(gulp.dest(conf.paths.dist));
 }
+
+const gutil = require('gulp-util');
+const through = require('through2');
+
+const regex = /\$ctrl.sim.(.*?)[" ]/g;
+
+function extractVars(file) {
+  gutil.log(gutil.colors.green(file.path));
+  let m;
+  const foundVars = [];
+  while ((m = regex.exec(file.contents)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+    foundVars.push(m[1]);
+  }
+  let previous = '';
+  foundVars.sort().forEach(v => {
+    if (v !== previous) {
+      gutil.log(v);
+      previous = v;
+    }
+  });
+}
+
+function simvars() {
+  return gulp.src([path.join(conf.paths.src, '/**/*.html')])
+    .pipe(through.obj((file, encoding, callback) => {
+      callback(null, extractVars(file));
+    }));
+}
+
+gulp.task('simvars', simvars);
