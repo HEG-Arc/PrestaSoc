3ef483690c721076e6061eeebbe0c8b28e6ab00f
Ulysse Rosselet <urosselet@gmail.com>
2017-02-06 00:39:27 +0100

    calcul subside Lamal ok

--- src/app/lamal/calcule.js
+++ src/app/lamal/calcule.js
@@ -7,7 +7,7 @@ class CalculeLamal {
     });
   }
 
-  rduLookup(menage, estEtudiant = 0, age, rdu) {
+  rduLookup(menage, estEtudiant = false, age, rdu) {
     const subside = this.subsides.find(x => {
       return x.menage === menage &&
         x.formation === estEtudiant &&
@@ -16,8 +16,12 @@ class CalculeLamal {
         x.rduMin <= rdu &&
         x.rduMax >= rdu;
     });
-    const subsideEstime = Math.round(rdu.subsideMin + (1 - (rdu - subside.rduMin) / (subside.rduMax - subside.rduMin)) * (subside.subsideMax - subside.subsideMin));
-    return {subsideMin: subside.subsideMin, subsideMax: subside.subsideMax, subsideEstime};
+    if (angular.isDefined(subside)) {
+      const subsideEstime = Math.round(subside.subsideMin + (1 - (rdu - subside.rduMin) / (subside.rduMax - subside.rduMin)) *
+                                                          (subside.subsideMax - subside.subsideMin));
+      return {subsideMin: subside.subsideMin, subsideMax: subside.subsideMax, subsideEstime};
+    }
+    return {subsideMin: 0, subsideMax: 0, subsideEstime: 0};
   }
 
   subsideLamalCalcule(rdu) {
@@ -39,9 +43,9 @@ class CalculeLamal {
   }
 
   calculRDU() {
-    const nombreEnfants = function (personnes) {
-      return personnes.reduce((count, person) => {
-        if (!person.estAdulte) {
+    const nombreEnfants = function (sim) {
+      return sim.personnes.reduce((count, person) => {
+        if (sim.age(person) < 18) {
           count++;
         }
         return count;
@@ -70,19 +74,34 @@ class CalculeLamal {
       const menageRDU = sim.personnes[0].etatCivil === 'C' ||
         sim.personnes[0].etatCivil === 'D' ||
         sim.personnes[0].etatCivil === 'V' ? "seul" : "couple";
-      let fortune = sim.fortuneImmobiliereLogement;
-      fortune -= Math.min(300000, sim.fortuneImmobiliereLogement);
-      fortune += sim.fortuneImmobiliereAutre;
-      fortune += sim.fortuneMobiliere;
+
+      let fortune = 0;
+      if (angular.isDefined(sim.fortuneImmobiliereLogement)) {
+        fortune += parseInt(sim.fortuneImmobiliereLogement, 10);
+        fortune -= Math.min(300000, parseInt(sim.fortuneImmobiliereLogement, 10));
+      }
+      if (angular.isDefined(sim.fortuneImmobiliereAutre)) {
+        fortune += parseInt(sim.fortuneImmobiliereAutre, 10);
+      }
+      if (angular.isDefined(sim.fortuneMobiliere)) {
+        fortune += parseInt(sim.fortuneMobiliere, 10);
+      }
       fortune -= franchiseFortune[menageRDU];
       fortune = Math.max(fortune, 0);
       return fortune * tauxMajoration;
     };
-    const nbEnfants = nombreEnfants(this.sim.personnes);
 
-    let rdu = this.sim.revenuNetImposable;
-    rdu += this.sim.rentePrevoyancePrivee;
-    rdu -= this.sim.fraisAccessoiresLogement;
+    let rdu = 0;
+    if (angular.isDefined(this.sim.revenuNetImposable)) {
+      rdu += parseInt(this.sim.revenuNetImposable, 10);
+    }
+    if (angular.isDefined(this.sim.rentePrevoyancePrivee)) {
+      rdu += parseInt(this.sim.rentePrevoyancePrivee, 10);
+    }
+    if (angular.isDefined(this.sim.fraisAccessoiresLogement)) {
+      rdu -= parseInt(this.sim.fraisAccessoiresLogement, 10);
+    }
+    const nbEnfants = nombreEnfants(this.sim);
     rdu -= reductionEnfants(nbEnfants);
     rdu += imputationFortune(this.sim);
     return rdu;
