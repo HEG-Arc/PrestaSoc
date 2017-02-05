1d85e8173b1b1a7cd91e05670e5abae05f75d093
Boris Fritscher <boris@fritscher.ch>
2017-02-05 19:03:49 +0100

    support binary variables in logement

--- src/app/base/logement.js
+++ src/app/base/logement.js
@@ -1,10 +1,27 @@
 class LogementController {
   /** @ngInject */
-  constructor(simulation, regions) {
+  constructor(simulation, regions, $scope) {
     this.sim = simulation;
     this.filterList = txt => {
       return regions.search(txt);
     };
+    $scope.$watch('$ctrl.sim.logement', () => {
+      if (this.logement === 'estLocataire') {
+        this.sim.estLocataire = true;
+        this.sim.estProprietaire = false;
+        this.sim.estEMS = false;
+      }
+      if (this.logement === 'estProprietaire') {
+        this.sim.estLocataire = false;
+        this.sim.estProprietaire = true;
+        this.sim.estEMS = false;
+      }
+      if (this.logement === 'estEMS') {
+        this.sim.estLocataire = false;
+        this.sim.estProprietaire = false;
+        this.sim.estEMS = true;
+      }
+    });
   }
 }
 
