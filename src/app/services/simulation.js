1ad702a175bded561502b00db964befeffc76909
Boris Fritscher <boris@fritscher.ch>
2017-02-06 21:43:16 +0100

    add missing updateEtudiants to simulation

--- src/app/services/simulation.js
+++ src/app/services/simulation.js
@@ -1,6 +1,11 @@
 class Simulation {
   constructor() {
     this.personnes = [];
+    this.updateEtudiants();
+  }
+
+  updateEtudiants() {
+    this.etudiants = this.personnes.filter(personne => personne.estEtudiant);
   }
 }
 
