7fefb0b4e097e55b8afe0f9c66b8e87d83524978
Ulysse Rosselet <urosselet@gmail.com>
2017-02-07 14:39:11 +0100

    removed duplicate updateEtudiants

--- src/app/services/simulation.js
+++ src/app/services/simulation.js
@@ -8,10 +8,6 @@ class Simulation {
     this.etudiants = this.personnes.filter(personne => personne.estEtudiant);
   }
 
-  updateEtudiants() {
-    this.etudiants = this.personnes.filter(x => x.estEtudiant);
-  }
-
 }
 
 export default Simulation;
