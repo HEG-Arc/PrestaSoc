b4e8bfe5095a19f36de0a71960c68e3894f5cd74
Boris Fritscher <boris@fritscher.ch>
2017-02-07 19:12:22 +0100

    nav for other screens

--- src/app/base/nav.js
+++ src/app/base/nav.js
@@ -5,6 +5,26 @@ class NavController {
     this.$state = $state;
   }
 
+  isBase() {
+    return this.$state.is('vous') || this.$state.is('foyer') || this.$state.is('logement');
+  }
+
+  isFinance() {
+    return this.$state.is('revenu') || this.$state.is('fortune');
+  }
+
+  isLamal() {
+    return this.$state.is('lamalEstimation');
+  }
+
+  isPC() {
+    return this.$state.is('pcDepenses') || this.$state.is('pcEstimation');
+  }
+
+  isBourse() {
+    return this.$state.includes('bourse') || this.$state.is('bourseEstimation');
+  }
+
   restart() {
     this.saver.restart();
     this.$state.go('vous');
