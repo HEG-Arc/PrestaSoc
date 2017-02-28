d19d7d6b3cea9fec293204976f68082762571f2b
Boris Fritscher <boris@fritscher.ch>
2017-02-28 10:40:19 +0100

    suport personne via src in inputContainer

--- src/app/base/inputContainer.js
+++ src/app/base/inputContainer.js
@@ -5,13 +5,28 @@ class InputContainerController {
     this.vars = vars;
     this.sim = simulation;
   }
+
+  $onChanges() {
+    if (this.var) {
+      const m = this.var.split('.');
+      this.model = this.sim;
+      if (this.src) {
+        this.model = this.src;
+      }
+      while (m.length > 1) {
+        this.model = this.model[m.shift()];
+      }
+      this.attribute = m.shift();
+    }
+  }
 }
 
 export const inputContainer = {
   template: require('./inputContainer.html'),
   controller: InputContainerController,
   bindings: {
-    var: '@'
+    var: '@',
+    src: '<'
   }
 };
 
