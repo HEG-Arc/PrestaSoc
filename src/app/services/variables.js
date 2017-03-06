bc2cae1a669c189ad0bddf66d54fbcde33bee958
Boris Fritscher <boris@fritscher.ch>
2017-03-06 11:36:21 +0100

    hide inputs until vars loaded fixes aria-label warning

--- src/app/services/variables.js
+++ src/app/services/variables.js
@@ -3,6 +3,7 @@ class Variables {
   constructor($http) {
     $http.get('https://script.google.com/macros/s/AKfycbxxtYoEsXArEobqe3Egs5-kWHawM45IggQYLczhUbcELDYg7Ag/exec').then(resp => {
       angular.copy(resp.data, this);
+      this._loaded = true;
     });
   }
 }
