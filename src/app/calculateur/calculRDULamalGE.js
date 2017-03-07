e9a7fb2305737c5059045e6775c054de111f6219
Ulysse Rosselet <urosselet@gmail.com>
2017-03-07 15:21:29 +0100

    calcul du subside GE

---
+++ src/app/calculateur/calculRDULamalGE.js
@@ -0,0 +1,6 @@
+
+export function calculRDU(sim) {
+  let rdu = 0;
+  rdu += 25000 + 0 * sim.revenuNetImposable;
+  return rdu;
+}
---
+++ src/app/calculateur/subsideLamalGE.js
@@ -0,0 +1,45 @@
+import {calculRDU} from './calculRDULamalGE';
+
+function subsideLookup(menage, nbEnfants, age, rdu, subsidesGERDU) {
+  let subside = {};
+  subside = subsidesGERDU.find(x => {
+    return x.menage === menage &&
+      x.nbEnfants === nbEnfants &&
+      x.ageMin <= age &&
+      x.ageMax >= age &&
+      x.rduMin <= rdu &&
+      x.rduMax >= rdu;
+  });
+  if (angular.isDefined(subside)) {
+    const obj = angular.copy(subside);
+    obj.subsideEstime = obj.subside;
+    obj.rduLAMAL = rdu;
+    return obj;
+  }
+  return {subsideMin: 0, subsideMax: 0, subsideEstime: 0};
+}
+
+export function subsideLamalCalculeGE(sim, subsidesGERDU) {
+  const rduLAMAL = calculRDU(sim);
+
+  if (!sim.lieuLogement) {
+    return 0;
+  }
+
+  const menage = sim.personnes[0].etatCivil === 'U' ||
+    sim.personnes[0].etatCivil === 'M' ||
+    sim.personnes[0].etatCivil === 'P' ? 'couple' : 'seul';
+  const nbEnfants = sim.personnes.filter(p => !p.estAdulte).length;
+
+  const subsideTotal = {subsideMin: 0, subsideMax: 0, subsideEstime: 0, rduLAMAL: 0};
+  for (let i = 0; i < sim.personnes.length; i++) {
+    const person = sim.personnes[i];
+    person.subsideLamal = subsideLookup(menage, nbEnfants, person.age, rduLAMAL
+      , subsidesGERDU);
+    subsideTotal.subsideEstime += person.subsideLamal.subsideEstime;
+    subsideTotal.subsideMax += person.subsideLamal.subsideEstime;
+    subsideTotal.rduLAMAL = rduLAMAL;
+  }
+  return subsideTotal;
+}
+
