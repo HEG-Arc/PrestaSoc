7e3ae8cc33ce0982dd3a7f96306d56eadf8aec3a
Ulysse Rosselet <urosselet@gmail.com>
2017-03-16 11:02:29 +0100

    calcul bourse

--- src/app/calculateur/bourseCalculNE.js
+++ src/app/calculateur/bourseCalculNE.js
@@ -5,6 +5,21 @@ import {calculRDU} from '../calculateur/calculRDUNE';
 const MAX_BOURSE = 24000;
 const MAX_BOURSE_INCREMENT_ENFANT_A_CHARGE = 6000;
 
+const FRAIS_FORMATION = {l2: 800, l3: 2200};
+
+const FRAIS_LOGEMENT_SEPARE_ENTRETIEN = 12310;
+const FRAIS_LOGEMENT_SEPARE = 6000;
+
+function FRAIS_REPAS_FORFAIT(lev) {
+  const FORFAIT_REPAS_JOUR = 10;
+  if (lev === "l2-Apprentissages") {
+    return 235 * FORFAIT_REPAS_JOUR; // disons que les apprentis ont 5 semaines de vacances
+  } else if (lev.substring(0, 2) === "l2") {
+    return 190 * FORFAIT_REPAS_JOUR; // le secondaire II a 14 semaines de vacances
+  }
+  return 150 * FORFAIT_REPAS_JOUR; // université: 30 semaines de cours /an
+}
+
 function fraisEntretien(nbPersonnes) {
   let fraisEntretien = 0;
   switch (nbPersonnes) {
@@ -22,9 +37,9 @@ function fraisEntretien(nbPersonnes) {
       break;
     case 7: fraisEntretien = 416;
       break;
-    default: fraisEntretien = 274 * nbPersonnes;
+    default: fraisEntretien = 274;
   }
-  return 1.15 * fraisEntretien; // REF http://rsn.ne.ch/DATA/program/books/rsne/htm/4181100.htm#_ftnref6
+  return 1.15 * fraisEntretien * nbPersonnes; // REF http://rsn.ne.ch/DATA/program/books/rsne/htm/4181100.htm#_ftnref6
 }
 
 function nombreEnfants(sim) {
@@ -40,13 +55,30 @@ export function bourseEtudeNE(sim) {
     const charges = [];
     const revenus = [];
     let montantBourse = 0;
+    const niveau = etudiant.niveauEtude.substring(0, 2);
 
 // charges
-    revenus.push(["Frais d'entretien", fraisEntretien(sim.personnes.length)]);
+    charges.push(["Frais d'entretien de la famille", fraisEntretien(sim.personnes.length)]);
+
+    // frais de logement: "Les frais effectifs de logement mais limités à un plafond dépendant du nombre de personnes et de sa localisation"
+    // pas trouvé le plafond dans l'Arrêté fixant les normes pour le calcul de l'aide matérielle, ni dans les autres sources citées
+    charges.push(["Frais de loyer de la famille", sim.logementLoyerBrut]);
+
+    charges.push(["Frais de formation", FRAIS_FORMATION[niveau]]);
+
+    if (etudiant.aLogementSepare) {
+      charges.push(["Frais d'entretien pour logement séparé", FRAIS_LOGEMENT_SEPARE_ENTRETIEN]);
+      charges.push(["Forfait loyer pour logement séparé", FRAIS_LOGEMENT_SEPARE]);
+    }
+
+    charges.push(["Forfait pour frais de repas", FRAIS_REPAS_FORFAIT[etudiant.niveauEtude]]);
+
 // revenus
     // répartition du RDU de l'UER entre les étudiants
     revenus.push(["Répartition du revenu déterminant unifié", Math.round(rdu / nbEtudiants)]);
 
+// TODO: salaire perçu par l'étudiant 80% des revenus obtenus dans le cadre de la formation après déduction d'un montant forfaitaire de 3'000 francs pour le secondaire II et de 4'800 francs pour le tertiaire
+// calcul de la balance du budget
     const chargesTotales = charges.reduce((total, charge) => total + charge[1], 0);
     const revenusTotaux = revenus.reduce((total, revenu) => total + revenu[1], 0);
     montantBourse = Math.min(revenusTotaux - chargesTotales, 0);
@@ -55,7 +87,7 @@ export function bourseEtudeNE(sim) {
     if (i === 0) { // enfants a charge de l'etudiant si celui-ci est le requérant principal
       maxBourse += nombreEnfants(sim) * MAX_BOURSE_INCREMENT_ENFANT_A_CHARGE;
     }
-    montantBourse = Math.max(montantBourse, maxBourse());
+    montantBourse = Math.min(montantBourse, maxBourse);
     montantBourse = 10 * Math.round(montantBourse / 10); // arrondi dizaine
     // TODO si le revenu est égal à l'ensemble des charges, l'OCBE octroie une aide pour les frais d'études uniquement;
     etudiant.bourseEtude = {charges, revenus, chargesTotales, revenusTotaux, montantBourse};
